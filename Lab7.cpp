a7fa124393f510f5200df1aa8801ac85957d4b79
User <User@DESKTOP-KOBUB7D>
2023-05-01 19:26:44 +0300

    Lab7

--- Lab7.cpp
+++ Lab7.cpp
@@ -1,20 +1,345 @@
-﻿// Lab7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <complex>
+#include <algorithm>
+using namespace std;
 
-#include <iostream>
+
+
+/*
+Написати функцію-шаблон послідовного пошуку в масиві по
+ключу. Функція повертає індекс останнього, знайденого елемента в
+масиві, рівного ключу. Написати специфікацію функції-шаблон для типу
+char*.
+*/
+
+template <typename T>
+int el(T v[], int n) {
+
+	T k;
+	int b = -1;
+	cout << "Input k: ";
+	cin >> k;
+
+	for (int i = 0; i < n; i++) {
+		if (v[i] == k) {
+			b = i;
+		}
+	}
+
+	return b;
+}
+template<int> char* el(char* v[], char* n)
+{
+	char k;
+	int b = -1;
+
+	cout << "Input k: ";
+	cin >> k;
+	cout << "v: ";
+	cout << v << "\n";
+
+
+	for (int i = 0; i < strlen(v[i]); i++) {
+		if (v[i] == (char*)k) {
+			b = i;
+		}
+	}
+
+	return b;
+}
+int main1()
+{
+	int V[] = { 1,2,3,4,5,5,6,7,8,9 };
+	float V1[] = { 1.1, 2.2, 3.3, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
+	char V3[] = { 'a','b','c','d','e','f','g','h','j','j' };
+
+	cout << "V: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V[i] << " ";
+	}
+	cout << endl;
+
+	int b1;
+	b1 = el(V, 10);
+	if (b1 == -1) { cout << "No" << endl; }
+	else
+		cout << "B: " << b1 << endl;
+
+
+	cout << "V1: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V1[i] << " ";
+	}
+	cout << endl;
+
+	float b2;
+	b2 = el(V1, 10.f);
+	if (b2 == -1) { cout << "No" << endl; }
+	else
+		cout << "B: " << b2 << endl;
+
+
+	cout << "V3: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V3[i] << " ";
+	}
+	cout << endl;
+
+
+	int b3;
+
+	b3 = el(V3, 10);
+	if (b3 == -1) { cout << "No" << endl; }
+	else
+		cout << "B: " << b3 << endl;
+
+	return 0;
+}
+
+
+/*
+Написати функцію-шаблон функцію впорядкування методом
+швидкого впорядкування. Написати специфікацію функції-шаблон для
+типу char*.
+*/
+
+template <class T1>
+T1* ell(T1 v[], int n) {
+	T1 a;
+	T1 t(10);
+
+	for (int i = 0; i < n - i; i++) {
+		if (v[i] > v[i + 1]) {
+			a = v[i];
+			v[i] = v[i + 1];
+			v[i + 1] = a;
+		}
+	}
+
+	return v;
+}
+char* ell1(char v[])
+{
+	size_t S = sizeof(v) / sizeof(*v);
+	sort(v, v + S);
+
+	return v;
+}
+int main2()
+{
+	int V[10] = { 2,4,3,1,8,5,4,2,8,6 };
+	float V1[] = { 2.0,2.4,2.1,5.0,3.0,5.4,6.2,6.0,2.0,5.8 };
+	char VV[] = { 'b','c','a','c','h','f','a','d','e','j' };
+
+	cout << "V: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V[i] << " ";
+	}
+	cout << endl;
+
+	cout << "V1: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V1[i] << " ";
+	}
+	cout << endl;
+
+	cout << "VV: ";
+	for (int i = 0; i < 10; i++) {
+		cout << VV[i] << " ";
+	}
+	cout << endl;
+
+	int* b1[10] = {};
+	*b1 = ell(V, 10);
+	cout << "B: " << b1 << endl;
+
+	float* b11[10] = {};
+	*b11 = ell(V1, 10);
+	cout << "B1: " << b11 << endl;
+
+	char* b2[10]{};
+	*b2 = ell1(VV);
+	cout << "B2: " << b2 << endl;
+
+	return 0;
+}
+
+
+/*
+Створити параметризований масив з конструкторами,
+деструктором і перевантаженими операторами [], =, +, +=,-,-=.
+*/
+
+template <class T3>
+T3* e(T3 v[], T3 n) {
+
+	for (int i = 0; i < n; i++) {
+		v[i] = 0;
+	}
+
+	return v;
+}
+template <class T3>
+T3 e1(T3 v[], T3 n) {
+
+	T3 k;
+	int b = -1;
+	cout << "Input k: ";
+	cin >> k;
+
+	for (int i = 0; i < n; i++) {
+		if (v[i] == k) {
+			b = v[i];
+		}
+	}
+
+	return b;
+}
+template <class T3>
+T3* e2(T3 v[], T3 n) {
+
+	return v;
+}
+template <class T3>
+T3* e3(T3 v[], T3 n, T3 vv[]) {
+
+	for (int i = 0; i < n; i++) {
+		v[i] = v[i] + vv[i];
+	}
+
+	return v;
+}
+template <class T3>
+T3* e4(T3 v[], T3 n, T3 vv[]) {
+
+	for (int i = 0; i < n; i++) {
+		v[i] += vv[i];
+	}
+
+	return v;
+}
+template <class T3>
+T3* e5(T3 v[], T3 n, T3 vv[]) {
+
+	for (int i = 0; i < n; i++) {
+		v[i] = v[i] - vv[i];
+	}
+
+	return v;
+}
+template <class T3>
+T3* e6(T3 v[], T3 n, T3 vv[]) {
+
+	for (int i = 0; i < n; i++) {
+		v[i] -= vv[i];
+	}
+
+	return v;
+}
+
+
+int main3()
+{
+	int V[] = { 1,2,3,4,5,5,6,7,8,9 };
+	int VV[] = { 9,8,7,6,5,5,4,3,2,1 };
+	float V1[] = { 1.1, 2.2, 3.3, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
+
+	cout << "V: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V[i] << " ";
+	}
+	cout << endl;
+
+	int* b[10];
+	*b = e(V, 10);
+	cout << "B: " << b << endl;
+
+	int b1;
+	b1 = el(V, 10);
+	if (b1 == -1) { cout << "No" << endl; }
+	else
+		cout << "[]: " << b1 << endl;
+
+
+	int* b2[10];
+	*b2 = e2(V, 10);
+	cout << "=: " << b2 << endl;
+
+	int* b3[10];
+	*b3 = e3(V, 10, VV);
+	cout << "+: " << b3 << endl;
+
+	int* b4[10];
+	*b4 = e4(V, 10, VV);
+	cout << "+=: " << b4 << endl;
+
+	int* b5[10];
+	*b5 = e5(V, 10, VV);
+	cout << "-: " << b5 << endl;
+
+	int* b6[10];
+	*b6 = e6(V, 10, VV);
+	cout << "-=: " << b6 << endl;
+
+
+	return 0;
+}
+
+
+
+/*
+Побудувати клас, що описує бінарне дерево стандартної форми.
+Побудувати клас ітератор, що дозволяє обходити дерево. Написати
+програму, яка проходить по бінарному дереву використовуючи ітератор.
+*/
+
+
+
+int main4()
+{
+	int V[] = { 1,2,3,4,5,5,6,7,8,9 };
+	float V1[] = { 1.0,2.0,2.1,3.0,5.0,5.4,6.2,7.0,8.0,9.0 };
+
+	cout << "V: ";
+	for (int i = 0; i < 10; i++) {
+		cout << V[i] << " ";
+	}
+	cout << endl;
+
+	int* b[10];
+	*b = e(V, 10);
+	cout << "B: " << b << endl;
+
+	int b1;
+	b1 = el(V, 10);
+	if (b1 == -1) { cout << "No" << endl; }
+	else
+		cout << "[]: " << b1 << endl;
+
+
+
+
+
+	return 0;
+}
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	int n;
+	cout << "N: ";
+e: cin >> n;
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	switch (n) {
+	case 1: main1(); break;
+	case 2: main2(); break;
+	case 3: main3(); break;
+	case 4: main4(); break;
+	default: cout << "Error\n"; goto e;
+	}
+	return 0;
+};
